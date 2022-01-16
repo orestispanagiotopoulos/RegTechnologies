989d904f4405ecfe40c8f700260770db68a1d121
orestispanagiotopoulos <orestis.panagiotopoulos@gmail.com>
2022-01-16 19:33:09 +0000

    Initial Api project

---
+++ MovieInfo/MovieInfo.Api/Contollers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieInfo.Api.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieInfo.Api.Contollers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            return Ok(MoviesDataStorage.Current.Movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+            // find city
+            var movieToReturn = MoviesDataStorage.Current.Movies
+                .FirstOrDefault(c => c.Id == id);
+
+            if (movieToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movieToReturn);
+        }
+    }
+}
